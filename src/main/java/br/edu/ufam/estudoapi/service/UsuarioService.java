e8642c0e99340d5aef17714790e0078f9064019c
Carlos Daniel <cdaniel01@gmail.com>
2023-09-19 23:01:52 -0400

    Refect: Este commit, se aplicado adiciona classe service.

---
+++ src/main/java/br/edu/ufam/estudoapi/service/UsuarioService.java
@@ -0,0 +1,20 @@
+package br.edu.ufam.estudoapi.service;
+
+import br.edu.ufam.estudoapi.domain.Usuario;
+import br.edu.ufam.estudoapi.repository.UsuarioRepository;
+import br.edu.ufam.estudoapi.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    public Usuario findById(Integer id){
+        Optional<Usuario> obj = usuarioRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+", Tipo: "+Usuario.class.getName()));
+    }
+}
