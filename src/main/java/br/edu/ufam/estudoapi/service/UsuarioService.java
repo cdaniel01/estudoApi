a23647e77c6692d985b4adddaa16a051b1cbb7d7
Carlos Daniel <cdaniel01@gmail.com>
2023-09-19 23:31:12 -0400

    Refect: Este commit, se aplicado adiciona o método que retorna a lista de usuarios.

--- src/main/java/br/edu/ufam/estudoapi/service/UsuarioService.java
+++ src/main/java/br/edu/ufam/estudoapi/service/UsuarioService.java
@@ -6,6 +6,7 @@ import br.edu.ufam.estudoapi.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,5 +17,9 @@ public class UsuarioService {
     public Usuario findById(Integer id){
         Optional<Usuario> obj = usuarioRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+", Tipo: "+Usuario.class.getName()));
+    }
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
     }
 }
