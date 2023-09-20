a854116f700da36a9101ee4add9cb7e22aad6aee
Carlos Daniel <cdaniel01@gmail.com>
2023-09-19 23:30:56 -0400

    Refect: Este commit, se aplicado adiciona o método que retorna a lista de usuarios.

--- src/main/java/br/edu/ufam/estudoapi/controller/UsuarioController.java
+++ src/main/java/br/edu/ufam/estudoapi/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioController {
@@ -20,5 +22,11 @@ public class UsuarioController {
 
         Usuario usuario = usuarioService.findById(id);
         return ResponseEntity.ok().body(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> usuarioList = usuarioService.findAll();
+        return ResponseEntity.ok().body(usuarioList);
     }
 }
